40ddac066688bf7082cb494794566db96f6ab48b
rickynyairo <rickynyairo@gmail.com>
2019-01-16 11:44:24 +0300

    added error handling functionality

--- dist/server.js
+++ dist/server.js
@@ -8,9 +8,19 @@ const express_1 = __importDefault(require("express"));
 const utils_1 = require("./utils");
 const services_1 = __importDefault(require("./services"));
 const middleware_1 = __importDefault(require("./middleware"));
+const errorHandlers_1 = __importDefault(require("./middleware/errorHandlers"));
+process.on("uncaughtException", e => {
+    console.log(e);
+    process.exit(1);
+});
+process.on("unhandledRejection", e => {
+    console.log(e);
+    process.exit(1);
+});
 const router = express_1.default();
 utils_1.applyMiddleware(middleware_1.default, router);
 utils_1.applyRoutes(services_1.default, router);
+utils_1.applyMiddleware(errorHandlers_1.default, router);
 const { PORT = 3000 } = process.env;
 const server = http_1.default.createServer(router);
 server.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`));
---
+++ dist/services/search/SearchController.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=SearchController.js.map
